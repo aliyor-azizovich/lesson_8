a450176c5b07cf0db453e8d6f61c8c1f7c3a0cdc
Aliyor Azizovich <aliyor.0276@gmail.com>
2023-12-15 15:08:37 +0500

    15.12 - finish commit

---
+++ visualization.py
@@ -0,0 +1,29 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import read_csv
+
+# df = pd.read_csv("california_housing_test.csv")
+# columns = df.columns
+# Изобразите отношение households к population с помощью точечного графика
+# sns.scatterplot(data = df, x = "households", y = "population")
+
+# Визуализировать longitude по отношения к median_house_value, используя линейный график
+
+# sns.lineplot(data=df, x="longitude", y="median_house_value")
+
+# Представить гистограмму по housing_median_age
+
+# plt.hist(df['housing_median_age'])
+
+# Изобразить гистограмму по median_house_value с оттенком housing_median_age
+
+# sns.histplot(data = df, x = 'median_house_value', y = 'housing_median_age')
+
+penguins = sns.load_dataset("penguins")
+print(penguins.head())
+
+
+plt.show()
+
